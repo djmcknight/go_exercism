ffbe747957abe6d2cd07cc0ac9096a79729e6ade
djmcknight <derek.mcknight@mcknightsolutions.io>
2021-04-26 12:11:57 -0400

    more effecient solution using bitwise operations

--- grains/grains.go
+++ grains/grains.go
@@ -1,21 +1,18 @@
 // Package grains will calculate the amount of grains on a given square
 package grains
 
-import "errors"
+import (
+	"errors"
+)
 
 // Square takes an int as input, and returns the amount of grains on that square
 func Square(sq int) (uint64,  error) {
-	var square uint64
-	square = 1
-	if sq == 1 {
-		return square, nil
-	}
+	var square uint64 = 1
+
 	if !(sq > -1) || sq > 64 || sq == 0 {
-		return 5, errors.New("number is Negative or greater than 64")
+		return 0, errors.New("number is Negative or greater than 64")
 	}
-	for i := 2; i <= sq; i++ {
-		square = square * 2
-	}
+	square = square << (sq -1)
 	return square, nil
 
 
@@ -23,11 +20,9 @@ func Square(sq int) (uint64,  error) {
 
 // Total returns the total amount of grains on the board.
 func Total() uint64 {
-	var totalSum uint64 = 1
-	var squareTotal uint64 = 1
-	for i := 2; i <= 64 ; i++ {
-		squareTotal = squareTotal * 2
-		totalSum += squareTotal
-	}
+	var totalSum uint64
+
+	totalSum = (1 << 63) + (1 << 63) - 1
 	return totalSum
+
 }
